f02a68911bc9cec2aae3dd361a81bd67adbb9560
XTENRACK <vozatv23@gmail.com>
2025-01-17 19:21:49 +0700

    Create sketch_jan17b.ino

---
+++ sketch_jan17b.ino
@@ -0,0 +1,129 @@
+/*Nodemcu ESP8266 WIFI control car.
+ * This code created by sritu hobby team.
+ * https://srituhobby.com
+ */
+ 
+#define BLYNK_TEMPLATE_ID "TMPL6rhL262ii"
+#define BLYNK_TEMPLATE_NAME "connect blynk"
+#define BLYNK_AUTH_TOKEN "NzspX2CtuTLZ7PkJjiVsFJm-ddgEVsOk"
+
+// Include necessary libraries
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include "esp32-hal-ledc.h"
+
+// Pin Definitions (Based on your setup)
+#define ENA 13  // Motor A Speed Control
+#define IN1 12  // Motor A Direction
+#define IN2 14  // Motor A Direction
+#define IN3 27  // Motor B Direction
+#define IN4 26  // Motor B Direction
+#define ENB 25  // Motor B Speed Control
+
+char ssid[] = "Veevi_2G";       // WiFi SSID
+char pass[] = "0860304069";     // WiFi Password
+
+
+bool forward = 0;
+bool backward = 0;
+bool left = 0;
+bool right = 0;
+int Speed;
+ 
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+ 
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+ 
+BLYNK_WRITE(V0) {
+  forward = param.asInt();
+}
+ 
+BLYNK_WRITE(V1) {
+  backward = param.asInt();
+}
+ 
+BLYNK_WRITE(V2) {
+  left = param.asInt();
+}
+ 
+BLYNK_WRITE(V3) {
+  right = param.asInt();
+}
+ 
+BLYNK_WRITE(V4) {
+  Speed = param.asInt();
+}
+ 
+void smartcar() {
+  if (forward == 1) {
+    carforward();
+    Serial.println("carforward");
+  } else if (backward == 1) {
+    carbackward();
+    Serial.println("carbackward");
+  } else if (left == 1) {
+    carturnleft();
+    Serial.println("carfleft");
+  } else if (right == 1) {
+    carturnright();
+    Serial.println("carright");
+  } else if (forward == 0 && backward == 0 && left == 0 && right == 0) {
+    carStop();
+    Serial.println("carstop");
+  }
+}
+void loop() {
+  Blynk.run();
+  smartcar();
+}
+ 
+void carforward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carbackward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carturnleft() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carturnright() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carStop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
